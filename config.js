bb361df3b40bbcfff61613252dc5349150750e26
yamiediewei <weiwei55nakata@vip.qq.com>
2019-01-08 03:26:48 +0800

    配置
    ddd

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 module.exports = {
   version: "5.0",
   note: '优化接口调用流程',
-  subDomain: "tz", // 如果你的域名是： https://api.it120.cc/abcd 那么这里只要填写 abcd
-  appid: "wxa46b09d413fbcaff", // 您的小程序的appid，购物单功能需要使用
+  subDomain: "weiwei", // 如果你的域名是： https://api.it120.cc/abcd 那么这里只要填写 abcd
+  appid: "wx88fd2942a2fc5a90", // 您的小程序的appid，购物单功能需要使用
   shareProfile: '百款精品商品，总有一款适合您' // 首页转发的时候话术
 }
 /*
